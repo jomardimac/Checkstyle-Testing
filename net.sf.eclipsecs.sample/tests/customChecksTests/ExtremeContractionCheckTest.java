906450a37a8e25f09e75ca7943a60aa9d050833e
djohnso5 <djohnso5@eecs.wsu.edu>
2017-09-29 14:03:55 -0700

    starting unit testing for ExtremeContraction

---
+++ net.sf.eclipsecs.sample/tests/customChecksTests/ExtremeContractionCheckTest.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.Before;
+
+public class ExtremeContractionCheckTest {
+	
+	public ExtremeContractionCheck ecc;
+
+	@Before
+	public void init() {
+		ecc = new ExtremeContractionCheck();
+	}
+
+	@Test
+	public void test() {
+		
+	}
+}
