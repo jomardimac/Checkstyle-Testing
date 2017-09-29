e60cf233cff8f092487ea2de7efd7d7126896524
djohnso5 <djohnso5@eecs.wsu.edu>
2017-09-29 13:14:19 -0700

    Adding comments to check (in progress)

--- net.sf.eclipsecs.sample/src/customChecks/ExtremeContractionCheck.java
+++ net.sf.eclipsecs.sample/src/customChecks/ExtremeContractionCheck.java
@@ -8,25 +8,26 @@ import antlr.collections.AST;
 
 public class ExtremeContractionCheck extends AbstractCheck {
 	
+	//Variable holding the minimum number of chars for a var, method, class, etc...
 	private int lowestAcceptableCount = 2;
 	
+	//Setter to change default lowest value for user.
 	public void setLowestAcceptableCount(int count) {
 		lowestAcceptableCount = count;
 	}
 
+	//Method that returns all the tokens needed for this check... Here we are looking at Classes, Interfaces, Methods, and Vars.
 	@Override
 	public int[] getDefaultTokens() {
 		return new int[] { TokenTypes.CLASS_DEF, TokenTypes.INTERFACE_DEF,
-				TokenTypes.METHOD_DEF};
+				TokenTypes.METHOD_DEF, TokenTypes.VARIABLE_DEF};
 	}
 	
+	//Method for behavior of check.
 	@Override
 	public void visitToken(DetailAST ast) {
-		DetailAST objBlock = ast.findFirstToken(TokenTypes.METHOD_DEF);
-		
-		log(ast.getLineNo(), "extremecontraction", 1);
-		
 		DetailAST child = (DetailAST) ast.getFirstChild();
+		
 		while(child != null) {
 			if (child.getText().length() < lowestAcceptableCount) {
 				log(ast.getLineNo(), "extremecontraction", 1);
