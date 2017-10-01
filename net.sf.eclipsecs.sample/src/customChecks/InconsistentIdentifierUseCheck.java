f2c720f1d9aebe10471f1768bfc2260dd94e67a2
djohnso5 <djohnso5@eecs.wsu.edu>
2017-09-30 21:54:27 -0700

    restarted eclipse a bunch and now it works.

---
+++ net.sf.eclipsecs.sample/src/customChecks/InconsistentIdentifierUseCheck.java
@@ -0,0 +1,41 @@
+package customChecks;
+
+import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+public class InconsistentIdentifierUseCheck extends AbstractCheck {
+
+private int lowestAcceptableCount = 2;
+	
+	//Setter to change default lowest value for user.
+	public void setLowestAcceptableCount(int count) {
+		lowestAcceptableCount = count;
+	}
+
+	//Method that returns all the tokens needed for this check... Here we are looking at Classes, Interfaces, Methods, and Vars.
+	@Override
+	public int[] getDefaultTokens() {
+		return new int[] { TokenTypes.CLASS_DEF, TokenTypes.INTERFACE_DEF,
+				TokenTypes.METHOD_DEF, TokenTypes.VARIABLE_DEF};
+	}
+	
+	//Method for behavior of check.
+	@Override
+	public void visitToken(DetailAST ast) {
+		DetailAST child = (DetailAST) ast.getFirstChild();
+		
+		//Traversing the AST
+		while(child != null) {
+			//Debugging output
+			System.out.println("text: " + child.getText() + "|" + child.getType());
+			//Checking to see if the length of the token is less than the tolerable amount
+			if (child.getText().length() < lowestAcceptableCount && child.getType() == 58) {
+				//Loggin error of type "extremecontraction" (defined within the messages.properties file), of size lowestAcceptableCount
+				log(ast.getLineNo(), "inconsistentidentifieruse", lowestAcceptableCount);
+			}
+			//Progressing in the tree.
+			child = (DetailAST) child.getNextSibling();
+		}
+	}
+}
