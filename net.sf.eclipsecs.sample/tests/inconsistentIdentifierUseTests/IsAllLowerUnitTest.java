ef7fc4f5fcc852ec2c6ad061210b232b49b91132
djohnso5 <djohnso5@eecs.wsu.edu>
2017-10-03 23:33:58 -0700

    realized that test was pointless. Done with Extreme contraction, moving onto inconsistent identifiers for unit

---
+++ net.sf.eclipsecs.sample/tests/inconsistentIdentifierUseTests/IsAllLowerUnitTest.java
@@ -0,0 +1,14 @@
+package inconsistentIdentifierUseTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IsAllLowerUnitTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
