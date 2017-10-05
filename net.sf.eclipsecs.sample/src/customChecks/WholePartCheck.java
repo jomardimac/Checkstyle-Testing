82c9be051c1d064dd12d71ae7a813722f18efa81
Jomar Dimaculangan <jdimacu1@eecs.wsu.edu>
2017-10-04 20:19:02 -0700

    Changes in wholepart

--- net.sf.eclipsecs.sample/src/customChecks/WholePartCheck.java
+++ net.sf.eclipsecs.sample/src/customChecks/WholePartCheck.java
@@ -6,12 +6,10 @@ import com.puppycrawl.tools.checkstyle.api.TokenTypes;
 
 import antlr.collections.AST;
 
-
-
 public class WholePartCheck extends AbstractCheck {
 
+	//returns the tokens for class, var, methods, interface.
 	public int[] getDefaultTokens() { 
-		// TODO Grab the class definitions
 		return new int[]{TokenTypes.CLASS_DEF, TokenTypes.INTERFACE_DEF, TokenTypes.METHOD_DEF, TokenTypes.VARIABLE_DEF};
 	}
 	
@@ -19,13 +17,12 @@ public class WholePartCheck extends AbstractCheck {
 	String clname = new String();
 	
 	@Override
-	public void visitToken(DetailAST ast) {
-		DetailAST objBlock = ast.findFirstToken(TokenTypes.METHOD_DEF);
-		DetailAST child = (DetailAST)ast.getFirstChild();
+	public void visitToken(DetailAST ast) {											//Called for each token visit.
+		DetailAST child = ast.getFirstChild();
 		
-		while(child != null) {
-						
-			//figure out when to grab the class name:
+		//Determine the type of child, then determine if that type is within the definition name.
+		/*
+		 * //figure out when to grab the class name:
 			if(child.getType() == 69 && foundCl == 0) { //child.getType() == 69 means a class is found.
 				foundCl = 1;
 				continue;
@@ -43,7 +40,7 @@ public class WholePartCheck extends AbstractCheck {
 				//System.out.println("text: "+ child.getText().toLowerCase() + " type: " + child.getType());
 				//The check to see if method is the class name:
 				if(child.getText().toLowerCase().contains(clname.toLowerCase()) == true) {
-					//System.out.println("This is false: " + child.getText());
+					System.out.println("This is false: " + child.getText());
 					log(ast.getLine(),"wholepartcheck");
 				}
 			}
@@ -51,7 +48,29 @@ public class WholePartCheck extends AbstractCheck {
 			//System.out.println("text: "+ child.getText().toLowerCase() + " type: " + child.getType());
 			//progress in the tree:
 			//System.out.println("movein");
-			child = (DetailAST) child.getNextSibling();
+		 */
+		while(child != null) {
+			//checks to see if its a human made type:
+			if(child.getType() == 58) {
+				//check to see if class name is found, if it is, store it.
+				if(foundCl == 0) {
+					clname = child.getText();
+					System.out.println("Class " + clname);
+					foundCl = 1;
+					continue;
+				}
+				//the actual illegal name:
+				if(child.getText().toLowerCase().contains(clname.toLowerCase())) {
+					System.out.println("illegal " + clname);
+					log(ast.getLineNo(), "wholepartcheck");
+				}
+				
+				//System.out.println("text: "+child.getText() + " Type: " + child.getType());
+			}
+			
+			
+			//System.out.println("text: "+child.getText() + " Type: " + child.getType());
+			child = child.getNextSibling();									//Progress child to next child in the 'tree'.
 			
 		}
 	}
