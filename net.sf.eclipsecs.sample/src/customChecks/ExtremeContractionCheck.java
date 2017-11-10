cbeba88fb392bb6650acff2540736280b88a1e9a
djohnso5 <djohnso5@eecs.wsu.edu>
2017-11-09 23:11:39 -0800

    refactored visit token for extreme contraction

--- net.sf.eclipsecs.sample/src/customChecks/ExtremeContractionCheck.java
+++ net.sf.eclipsecs.sample/src/customChecks/ExtremeContractionCheck.java
@@ -2,6 +2,8 @@
 
 package customChecks;
 
+import java.util.ArrayList;
+
 import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
 import com.puppycrawl.tools.checkstyle.api.TokenTypes;
@@ -38,10 +40,10 @@ public class ExtremeContractionCheck extends AbstractCheck {
 				TokenTypes.METHOD_DEF, TokenTypes.VARIABLE_DEF};
 	}
 	
-	//Method for behavior of check.
-	@Override
-	public void visitToken(DetailAST ast) {
+	//Extra helper method to ease testing for visitToken
+	public ArrayList<Integer> findLinesWithErrors(DetailAST ast) {
 		DetailAST child = (DetailAST) ast.getFirstChild();
+		ArrayList<Integer> lineNumbers = new ArrayList<Integer>();
 		
 		//Traversing the AST
 		while(child != null) {
@@ -49,11 +51,24 @@ public class ExtremeContractionCheck extends AbstractCheck {
 			System.out.println("text: " + child.getText() + "|" + child.getType());
 			//Checking to see if the length of the token is less than the tolerable amount
 			if (child.getText().length() < lowestAcceptableCount && child.getType() == 58) {
-				//Logging error of type "extremecontraction" (defined within the messages.properties file), of size lowestAcceptableCount
-				log(ast.getLineNo(), "extremecontraction", lowestAcceptableCount);
+				lineNumbers.add(child.getLineNo());
 			}
 			//Progressing in the tree.
 			child = (DetailAST) child.getNextSibling();
+		}
+		
+		return lineNumbers;
+	}
+	
+	//Method for behavior of check.
+	@Override
+	public void visitToken(DetailAST ast) {
+		//Calling helper function to get all line numbers of invalid vars
+		ArrayList<Integer> lineNumbers = findLinesWithErrors(ast);
+		//Adding errors to all lines that didn't meet the parameters
+		for(int x : lineNumbers) {
+			//Logging error of type "extremecontraction" (defined within the messages.properties file), of size lowestAcceptableCount
+			log(x, "extremecontraction", lowestAcceptableCount);
 		}
 	}
 }
